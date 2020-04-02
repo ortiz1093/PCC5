cd9d7b59c8230d3ee806a59988e4a0a32898bcb4
Josh Ortiz <ortiz1093@yahoo,com>
2020-04-02 17:35:20 -0400

    First commit

---
+++ PCC5.py
@@ -0,0 +1,10 @@
+"""
+Python Competency Check 5
+IE 6910: Python Machine Learning Applications in IE
+Thu Apr  2 17:01:07 2020
+Joshua Ortiz
+"""
+
+import pandas as pd
+
+df = pd.read_csv("Bread Composition.csv")
