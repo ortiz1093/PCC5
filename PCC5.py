c58b35dd9151ab65f69dfb6d25fd30e9912258e3
Josh Ortiz <ortiz1093@yahoo,com>
2020-04-02 21:58:30 -0400

    Finished histogram and initial stats

--- PCC5.py
+++ PCC5.py
@@ -6,7 +6,30 @@ Joshua Ortiz
 """
 
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv("Bread Composition.csv")
 
-## Statistical Cleanup and Visualization
+#%% Statistical Cleanup and Visualization
+
+# Check nan
+print(df[df==np.nan].sum())
+
+# Statistical Summary
+print(df.describe)
+
+# Tabulate quality versus other categories
+qualityMeans = df.groupby("quality").mean()
+
+# Correlation of each category with quality (sorted according to abs val)
+qualityCorr = abs(df.corrwith(df.quality).drop(labels="quality")).sort_values()
+print(qualityCorr)
+
+# Capture the top factors from the correlation list
+significantFactors = qualityCorr.index[-4:]
+
+# Create histogram of significant factors from main data
+df.loc[:,significantFactors].hist(figsize=(15,15))
+
+# Plot significant factors against quality to verify trends exist
+qualityMeans.loc[:,significantFactors].plot.line(figsize=(15,15),subplots=True)
